91440bb6e833158195e131ec48c6d434aa00bb19
antoine-baptista <95496423+antoine-baptista@users.noreply.github.com>
2022-02-21 13:55:10 +0100

    Create App.vue

---
+++ Compétence-3/fanzine-de-breizh-vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view/>
+</template>
+
+<style>
+
+/* 
+document.getElementById('lien').href ="https://www.1formatik.com";
+document.getElementById("lien").textContent = "Aller sur 1FORMATIK.com";
+*/
+
+/* Lignes ci-dessous commentées, il s'agit des lignes du projet Vue
+d'origine */
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+} */
+</style>
