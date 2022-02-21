27ca4dd75a438f6a7b9feb2fe8a9e8a32c54f70d
antoine-baptista <95496423+antoine-baptista@users.noreply.github.com>
2022-02-21 13:55:24 +0100

    Create Header.vue

---
+++ Compétence-3/fanzine-de-breizh-vue/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- component Ok -->
+    <div class="entete-de-page">
+        <router-link id="mcdo" to="/menu"><img src="../assets/menu.png" width="40" height="40"></router-link>
+        <!-- <a id="mcdo" href="./menu"><img src="../assets/menu.png" width="40" height="40"></a> -->
+        <div class="nom-blog"><h1>Fanzine de Breizh</h1></div>
+        <div class="hermine"><img src="../assets/logo.png" alt="Hermine stylisée" width="40" height="40"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+   
+  }
+}
+</script>
+
