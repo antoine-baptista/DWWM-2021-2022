9eac1022d5427d33ccd3f49f23e0fa09b0b8150d
antoine-baptista <95496423+antoine-baptista@users.noreply.github.com>
2022-02-21 13:57:03 +0100

    Create Menu.vue

---
+++ Compétence-3/fanzine-de-breizh-vue/src/views/Menu.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="menu">
+    <Headerarrow/>
+    <main id="menuindex">
+        <h2 class="blog">Blog :</h2>
+        <img src="../assets/triangle.png" alt="Triangle" width="90" height="90">
+    </main>
+    <Navigationmobile/>
+    <Networks/>
+    <Footer/>
+  </div>
+</template>
+
+
+
+<script>
+
+// @ is an alias to /src
+import Headerarrow from '@/components/Headerarrow.vue'
+import Navigationmobile from '@/components/Navigationmobile.vue'
+import Networks from '@/components/Networks.vue'
+import Footer from '@/components/Footer.vue'
+document.title="Fanzine de Breizh | Menu";
+
+export default {
+  name: 'Menu',
+  components: {
+    Headerarrow,
+    Navigationmobile,
+    Networks,
+    Footer
+    
+  }
+}
+
+</script>
+
+
+<style>
+
+Body{
+  /* on affecte à l'élément 'Body' une image de fond */
+  background-image: url('/img/sable.jpg');
+  background-attachment: fixed;
+  background-position-y: bottom;
+  background-size: cover;
+  background-repeat: no-repeat;
+  overflow: hidden;
+  }
+
+
+
+</style>
+
