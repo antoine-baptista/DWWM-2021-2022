98bac337053ebb71356da59c9d56285b9da719b2
antoine-baptista <95496423+antoine-baptista@users.noreply.github.com>
2022-02-21 13:57:00 +0100

    Create Networks.vue

---
+++ Compétence-3/fanzine-de-breizh-vue/src/components/Networks.vue
@@ -0,0 +1,21 @@
+<template>
+<!-- component Ok -->
+    <div class="reseaux">
+      <nav id="tous-les-reseaux">
+           <a href="mailto:fanzinedebreizh@gmail.com" title="Me contacter" target="_blank"><img src="../assets/gmail.png" alt="Gmail"></a>
+           <a href="https://www.linkedin.com/in/awenbreizh/" title="Mon profil Linkedin" target="_blank"><img src="../assets/linkedin.png" alt="Linkedin"></a>
+           <a href="https://www.youtube.com/channel/UCLjOzL3O5LwtDbMVhNVfc1g" title="Ma chaine YouTube" target="_blank"><img src="../assets/youtube.png" alt="YouTube"></a>
+           <a href="https://twitter.com/fanzinedebreizh" title="Mon profil Twitter" target="_blank"><img src="../assets/twitter.png" alt="Twitter"></a>
+           <a href="http://www.locamandigital.art/" title="Mon site Web" target="_blank"><img src="../assets/web.png" alt="Web"></a>
+    </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Reseaux',
+  props: {
+   
+  }
+}
+</script>
