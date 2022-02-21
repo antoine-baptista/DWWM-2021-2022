04e0c94134ffc1d490f78f0c33263ba26adf0868
antoine-baptista <95496423+antoine-baptista@users.noreply.github.com>
2022-02-21 13:55:54 +0100

    Create Footer.vue

---
+++ Compétence-3/fanzine-de-breizh-vue/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+<!-- component Ok -->
+    <div class="pied-de-page">
+        <p>Copyright ©<span> Fanzine de Breizh </span> 2021-2022.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+   
+  }
+}
+</script>
