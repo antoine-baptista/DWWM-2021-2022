f3d6347e315ed2614e64366a67d5a4b0460bd402
antoine-baptista <95496423+antoine-baptista@users.noreply.github.com>
2022-02-21 13:55:56 +0100

    Create Headerarrow.vue

---
+++ Compétence-3/fanzine-de-breizh-vue/src/components/Headerarrow.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- component Ok -->
+    <div class="entete-de-page">
+        <router-link id="mcdofleche" to="/"><img src="../assets/left-arrow.png" width="40" height="40"></router-link>
+        <!-- <a id="mcdo" href="./menu"><img src="../assets/menu.png" width="40" height="40"></a> -->
+        <div class="nom-blog"><h1>Fanzine de Breizh</h1></div>
+        <div class="hermine"><img src="../assets/logo.png" alt="Hermine stylisée" width="40" height="40"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Headerarrow',
+  props: {
+   
+  }
+}
+</script>
+
